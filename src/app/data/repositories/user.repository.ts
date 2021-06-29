fc18c35c2bc7272c288c047092b939f539912864
darwinj94506 <darwinj94@hotmail.com>
2021-06-29 08:51:22 -0500

    feat: create initial project structure

---
+++ src/app/data/repositories/user.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { BaseRepository } from './base.repository';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRepository extends BaseRepository {
+
+  constructor() { 
+    super()
+  }
+
+}
